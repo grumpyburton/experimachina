0059a4a64992a2ddfb53feb6a0fd255b01b877c7
Alex Burton <grumpy.burton@gmail.com>
2024-02-14 16:00:14 +1100

    initial checkin

---
+++ api-experi-machina/src/main/java/au/com/cba/apiexperimachina/ApiExperiMachinaApplication.java
@@ -0,0 +1,13 @@
+package au.com.cba.apiexperimachina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExperiMachinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiExperiMachinaApplication.class, args);
+	}
+
+}
---
+++ api-experi-machina/src/test/java/au/com/cba/apiexperimachina/ApiExperiMachinaApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.cba.apiexperimachina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExperiMachinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
